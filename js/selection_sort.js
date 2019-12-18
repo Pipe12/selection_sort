cf19124f6bdeb7f5c118f0c1dcd807115a5b337b
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2019-12-18 11:51:22 -0500

    Create a function selection sort algorithm

--- js/selection_sort.js
+++ js/selection_sort.js
@@ -1,1 +1,27 @@
-alert("Sara te amo!!!");
+//Selection sort function
+
+function selectionSort(arrayToSort) {
+  var smallest;
+  var temp;
+
+  for (var i = 0; i < numbers.length - 1; i++) {
+    smallest =  i;
+    for (var j = i + 1; j < numbers.length; j++) {
+      if (arrayToSort[smallest] > arrayToSort[j]) {
+        smallest = j;
+      }
+    }
+    temp = arrayToSort[i];
+    arrayToSort[i] = arrayToSort[smallest];
+    arrayToSort[smallest] = temp;
+  }
+  return arrayToSort;
+}
+
+//Set variables
+
+const numbers = [12, 9, 3, 7, 14, 11];
+
+//Linear procedure
+console.log("initial sort = " + numbers);
+console.log(selectionSort(numbers));
